ed800ef40e16e92e7940bf0d2afd020983623947
Eden Vandewatyne <eden.vandewatyne@estiam.com>
2025-06-02 12:53:17 +0200

    Ajout d'une recherche de video grace a yt_dlp

--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,7 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import yt_dlp
 
 load_dotenv()
 
@@ -10,6 +11,17 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="?", intents=intents)
 token = os.getenv("DISCORD_BOT_TOKEN")
+
+ytdl_format_options = {
+    "format": "bestaudio/best",
+    "quiet": True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'no_warnings': True,
+    'default_search': 'ytsearch'
+}
+
+ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
 
 @bot.event
 async def on_ready():
@@ -20,7 +32,16 @@ async def ping(ctx):
     await ctx.send("Pong!")
 
 @bot.command()
-async def baiseur(ctx):
-    await ctx.send("Je suis Loic Arthur NKWINDJA BIEDA")
+async def play(ctx, *, search):
+    try:
+        info = ytdl.extract_info(f"ytsearch:{search}", download=False)["entries"][0]
+        url = info["webpage_url"]
+        titre = info['title']
+        
+        await ctx.send(f"Voici ce que j'ai trouvé pour **{search}** :\n{titre}\n{url}")
+
+    except Exception as e:
+        await ctx.send("Désolé, je n'ai rien trouvé.")
+        print(f"Erreur play : {e}")
 
 bot.run(token)
